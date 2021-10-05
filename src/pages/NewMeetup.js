1a3777414c5ff9c0b86ca5ccaa08ee71fe8809cd
BryanBudelon <bryan_dorneles@hotmail.com>
2021-10-06 10:50:59 +1100

    replace state

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,11 @@
+import { useHistory } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() { 
+
+    const history = useHistory();
+
     function addMeetupHandler(meetupData) {
         fetch(
             'https://react-getting-started-7d3c9-default-rtdb.firebaseio.com/meetups.json',
@@ -11,7 +16,9 @@ function NewMeetupPage() {
                     'Content-Type': 'application/json'
                 }
             }
-        )
+        ).then(() => {
+            history.replace('/');
+        });
     }
     
     return (
